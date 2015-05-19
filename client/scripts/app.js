7683f7256b4b79db67d5605f7f7c859782ef0c41
mliq <liquori@gmail.com>
2015-05-19 15:18:51 -0500

    first route working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('WelcomeController',["$scope", function($scope){
+myApp.controller('WelcomeController',["$scope", "$http", function($scope,$http){
     $scope.heading = "Message: ";
 
     $scope.updateMessage = function(){
@@ -10,4 +10,16 @@ myApp.controller('WelcomeController',["$scope", function($scope){
     $scope.goodByeMessage = function(){
         $scope.message = "Goodbye, " + $scope.name;
     };
+
+    $scope.grabinfo = function(){
+        return $http.get('/info').then(function(response){
+            if(response.status !== 200)
+            {
+                throw new Error("I haz sad.");
+            }
+
+            console.log(response);
+            return response.data;
+        });
+    }
 }]);
